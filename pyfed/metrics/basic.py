317699e4d11f78ff1dc7592aa01e5e648dced881
Dev-Jahn <ajh508@naver.com>
2023-03-31 04:06:53 +0900

    Added ClassificationMetric class

--- pyfed/metrics/basic.py
+++ pyfed/metrics/basic.py
@@ -1,7 +1,122 @@
+from functools import cache, lr_lru_cache_wrapper
+
 import numpy as np
 import torch
-from sklearn.metrics import confusion_matrix
+import sklearn
 import plotly.graph_objects as go
+
+
+# TODO: Add multi-label classification support
+class ClassificationMetrics:
+    """
+    Continuously stores and computes metrics for classification task
+    Efficient implementation for n>>d case, when n is n_samples and d is n_classes
+    """
+
+    def __init__(self, n_classes, pos_label=1, average='binary'):
+        if n_classes > 2:
+            assert average in [None, 'micro', 'macro', 'weighted']
+        self.n_classes, self.pos_label, self.average = n_classes, pos_label, average
+        self.reset()
+
+    def clear_caches(self):
+        props = list(map(lambda x: x.fget,
+                         filter(lambda attr: isinstance(attr, property), ClassificationMetrics.__dict__.values())))
+        attrs = list(ClassificationMetrics.__dict__.values())
+        for wrapper in filter(lambda attr: isinstance(attr, _lru_cache_wrapper), attrs + props):
+            wrapper.cache_clear()
+
+    def reset(self):
+        self.confusion = np.zeros((self.n_classes, self.n_classes))
+        self.clear_caches()
+
+    def update(self, true, pred):
+        self.confusion += sklearn.metrics.confusion_matrix(true, pred)
+        self.clear_caches()
+
+    @cache
+    def accuracy(self):
+        return self.confusion.trace() / self.confusion.sum()
+
+    @property
+    @cache
+    def _pred(self):
+        return self.confusion.sum(axis=0)
+
+    @property
+    @cache
+    def _true(self):
+        # can be viewed as 'support'
+        return self.confusion.sum(axis=1)
+
+    @property
+    @cache
+    def _tp(self):
+        return np.diag(self.confusion)
+
+    @property
+    @cache
+    def _fp(self):
+        return self._pred - self._tp
+
+    @property
+    @cache
+    def _fn(self):
+        return self._true - self._tp
+
+    def _metrics_impl(self, op):
+        assert op in ['prec', 'recl', 'f1']
+        if op == 'prec':
+            denom = self._pred
+        if op == 'recl':
+            denom = self._true
+        if op == 'f1':
+            denom = (self._pred + self._true) / 2
+        if np.any(denom == 0):
+            raise ArithmeticError('Precision, recall and F-1 score when denominator is zero is not defined.')
+
+        raw = self._tp / denom
+        if self.average is None:
+            return raw
+        elif self.average == 'binary':
+            return raw[self.pos_label]
+        elif self.average == 'micro':
+            return self.accuracy()  # may differ when multi-label
+        elif self.average == 'macro':
+            return raw.mean()
+        elif self.average == 'weighted':
+            return np.average(raw, weights=self._true)
+
+    @cache
+    def precision(self):
+        return self._metrics_impl('prec')
+
+    @cache
+    def recall(self):
+        return self._metrics_impl('recl')
+
+    @cache
+    def f1(self):
+        return self._metrics_impl('f1')
+
+    def summary(self, prefix=None):
+        prefix = '' if prefix is None else prefix + ' '
+        return {
+            prefix + 'Accuracy': self.accuracy(),
+            prefix + 'Precision': self.precision(),
+            prefix + 'Recall': self.recall(),
+            prefix + 'F-1 Score': self.f1()
+        }
+
+    @staticmethod
+    def _test():
+        true, pred = np.random.randint(0, 10, (10000)), np.random.randint(0, 10, (10000))
+        metrics = ClassificationMetrics(10, average=None)
+        metrics.update(true, pred)
+        return np.allclose(sklearn.metrics.accuracy_score(true, pred), metrics.accuracy()) and \
+            np.allclose(sklearn.metrics.precision_score(true, pred, average=None), metrics.precision()) and \
+            np.allclose(sklearn.metrics.recall_score(true, pred, average=None), metrics.recall()) and \
+            np.allclose(sklearn.metrics.f1_score(true, pred, average=None), metrics.f1())
 
 
 class AverageMeter(object):
@@ -49,7 +164,7 @@ def compute_accuracy(model, dataloader, get_confusion_matrix=False, device='cuda
                 true_labels_list = np.append(true_labels_list, target.data.cpu().numpy())
 
     if get_confusion_matrix:
-        conf_matrix = confusion_matrix(true_labels_list, pred_labels_list)
+        conf_matrix = sklearn.metrics.confusion_matrix(true_labels_list, pred_labels_list)
 
     if was_training:
         model.train()
